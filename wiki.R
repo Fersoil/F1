2e03a1eec702d7c39a20389e45c8f5eef31723f9
wierzchw <w.wierzch@gmail.com>
2022-10-23 13:53:25 +0200

    wykres tenis-like

--- wiki.R
+++ wiki.R
@@ -16,16 +16,50 @@ for (el in data_set_names ){
 library(dplyr)
 library(ggplot2)
 
-# hamilton niebieski
-# schumacher czerwony
-# verstappen granatowy
-# vettel zielony
+najlepsi_id <- drivers %>%
+  filter(surname %in% c("Hamilton", "Schumacher", "Vettel", "Verstappen"))
 
-# filtruje z resoults tylko tych kierowców co nas interesuj¹ i tylko pierwsze 
+# hamilton niebieski id=1
+# schumacher czerwony id=30
+# verstappen granatowy id=830
+# vettel zielony id=20
+head(df)
+
+# filtruje z results tylko tych kierowców co nas interesuj¹ i tylko pierwsze 
 # miejsca
+results %>%
+  filter(driverId %in% c(1, 30, 830, 20) & position == 1) %>%
+  select(driverId, raceId) -> df
 
 # merge z races po raceid zeby miec rok
+races %>%
+  select(raceId, year) %>%
+  merge(df) -> df
 
 # grupuje sezonami i kierowcami, zliczam
+df %>%
+  group_by(year) %>%
+  count(driverId) -> df
+  
+df %>%
+  group_by(driverId) %>%
+  mutate(cumsum(n)) -> df
 
+# okazuje sie ze numeryczne driverId sie nie spisuje X,D 
+df %>%
+  ggplot(aes(x = year, y = `cumsum(n)`, colour = driverId)) + 
+  geom_line() + 
+  geom_point() 
 
+#popraweczka
+df$driverId <- as.character(df$driverId)
+
+#goodgame plot
+df %>%
+  ggplot(aes(x = year, y = `cumsum(n)`, colour = driverId)) + 
+  geom_line() + 
+  geom_point() +
+  labs(title = "liczba zwyciêzonych wyœcigów na przestrzeni kariery", 
+       x = "sezony",
+       y = "sumaryczna liczba zwyciêstw") +
+  xlim(1990, 2022)
