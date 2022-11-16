dfd8d93123179d31188990014543a784fe218680
nizwant <mateusznizwantowski@gmail.com>
2022-11-16 11:18:32 +0100

    Schumacher color

--- mega_wazny_plik.R
+++ mega_wazny_plik.R
@@ -3,9 +3,9 @@
 Hamilton <- "#00c2cb"
 Vettel <- "#057937"
 Verstappen <- "#005bd5"
-Schumacher <- "#e20404"
+Schumacher <- "#e22020"
 
 driver_colors <- c("Lewis Hamilton" = "#00c2cb", "Sebastian Vettel" = "#057937", 
-                   "Max Verstappen" = "#005bd5", "Michael Schumacher" = "#e20404")
+                   "Max Verstappen" = "#005bd5", "Michael Schumacher" = "#e22020")
 
 our_drivers <- c("Lewis Hamilton", "Max Verstappen", "Sebastian Vettel", "Michael Schumacher")
